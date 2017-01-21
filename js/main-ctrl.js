72f8e7eba3486d32d6f7aeae0005009dbf9ed0db
Uri Kalish <uri.kalish@gmail.com>
2017-01-21 02:42:21 +0200

    continue

--- js/main-ctrl.js
+++ js/main-ctrl.js
@@ -1,6 +1,8 @@
 angular.module('mainApp').controller('mainCtrl', function mainCtrl($scope, $document, $timeout, mainServ) {
 
   $scope.model = {
+    helpText: mainServ.getHelpText(),
+    message: 'Throw the sticks'
   };
 
 });
